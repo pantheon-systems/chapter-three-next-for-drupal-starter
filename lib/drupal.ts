f6a79f02f848463f6e33b3eefc4cc3fa0c09d94b
shadcn <arshad@chummun.com>
2022-09-08 21:15:25 +0400

    feat: implement PANTHEON_CMS_ENDPOINT

--- lib/drupal.ts
+++ lib/drupal.ts
@@ -1,14 +1,11 @@
 import { DrupalClient } from "next-drupal"
 
-export const drupal = new DrupalClient(
-  process.env.NEXT_PUBLIC_DRUPAL_BASE_URL,
-  {
-    previewSecret: process.env.DRUPAL_PREVIEW_SECRET,
-    auth: {
-      clientId: process.env.DRUPAL_CLIENT_ID,
-      clientSecret: process.env.DRUPAL_CLIENT_SECRET,
-    },
-    // @see https://next-drupal.org/docs/configuration#forceiframesamesitecookie
-    forceIframeSameSiteCookie: process.env.NODE_ENV === "development",
-  }
-)
+export const drupal = new DrupalClient(process.env.BACKEND_URL, {
+  previewSecret: process.env.DRUPAL_PREVIEW_SECRET,
+  auth: {
+    clientId: process.env.DRUPAL_CLIENT_ID,
+    clientSecret: process.env.DRUPAL_CLIENT_SECRET,
+  },
+  // @see https://next-drupal.org/docs/configuration#forceiframesamesitecookie
+  forceIframeSameSiteCookie: process.env.NODE_ENV === "development",
+})
--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
+// Set some environment variables to work with Pantheon.
+process.env.BACKEND_URL = `https://${process.env.PANTHEON_CMS_ENDPOINT}`
+process.env.NEXT_PUBLIC_DRUPAL_BASE_URL = process.env.BACKEND_URL
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [process.env.NEXT_IMAGE_DOMAIN],
+    domains: [process.env.IMAGE_DOMAIN || process.env.PANTHEON_CMS_ENDPOINT],
   },
   output: "standalone",
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
